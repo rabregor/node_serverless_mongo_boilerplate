884b4e596c75514e52e04fe9c23b3dfe543aaa95
“miguelmendozajr” <miguelmendozaj20@gmail.com>
2023-10-16 10:33:21 -0600

    fix: auth file needed database connection

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import { SECRET_KEY } from "../config/environment.js";
 import responses from "../utils/responses.js";
+import connectDB from "../utils/connect.js";
 
 export const authenticateJWT = async (request) => {
   const { headers } = request.event;
@@ -12,6 +13,7 @@ export const authenticateJWT = async (request) => {
   const [, receivedToken] = token.split(" ");
   try {
     const user = jwt.verify(receivedToken, SECRET_KEY);
+    await connectDB();
     request.context.user = user;
   } catch (error) {
     return responses.internalError(error);
