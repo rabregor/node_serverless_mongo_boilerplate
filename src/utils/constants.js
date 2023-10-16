bc1bdc437a6aeaed610186ca4d2284be714fa560
“miguelmendozajr” <miguelmendozaj20@gmail.com>
2023-10-16 10:27:13 -0600

    added mongo options

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,13 +1,6 @@
-const dynamoConfig = {
-  region: "us-east-1",
-  endpoint: "http://localhost:8000",
-  tables: {
-    user: "nips_users",
-    file: "nips_files",
-    organization: "nips_organizations",
-    requirement: "nips_requirements",
-    folder: "nips_folders",
-  },
+const mongoOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 };
 
-export { dynamoConfig };
+export { mongoOptions };
